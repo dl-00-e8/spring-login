230a44353f5587cce36d130f79bc5f6964d2b26b
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-09-20 00:33:31 +0900

    Feat: Controller 기본 API 생성

--- single/src/main/java/com/login/practice/domain/user/controller/UserController.java
+++ single/src/main/java/com/login/practice/domain/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.login.practice.domain.user.controller;
 
+import com.login.practice.domain.user.dto.UserReq;
 import com.login.practice.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody UserReq userReq) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(null);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<Void> signin(@RequestBody UserReq userReq) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(null);
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<Void> signout(@RequestBody UserReq userReq) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(null);
+    }
+
+    @PostMapping("/withdrawal")
+    public ResponseEntity<Void> withdrawal(@RequestBody UserReq userReq) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(null);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<Void> reissue() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(null);
+    }
 }
