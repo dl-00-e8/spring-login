6e2056aefdb7068f2c9a2ae893b72423c3dce943
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-24 02:07:57 +0900

    Feat: JwtFilter에서 에러 발생 시 처리하기 위한 ExceptionFilter 구현

--- single/src/main/java/com/login/practice/global/filter/ExceptionFilter.java
+++ single/src/main/java/com/login/practice/global/filter/ExceptionFilter.java
@@ -1,7 +1,10 @@
 package com.login.practice.global.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.login.practice.global.exception.TokenException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,6 +22,17 @@ public class ExceptionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (TokenException e) {
+            setErrorResponse(response, e.getMessage());
+        }
+    }
 
+    private void setErrorResponse(HttpServletResponse response, String e) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), e); // 반환 객체 커스텀 필요
     }
 }
