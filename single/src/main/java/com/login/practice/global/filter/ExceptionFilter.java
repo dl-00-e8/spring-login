e8fbc19f68afcf59f4ea21fe94e3b2eb166d8514
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-24 01:23:03 +0900

    Feat: JwtFilter에서 Exception 발생 시 관리하기 위한 ExceptionFilter 기본 포맷 생성

---
+++ single/src/main/java/com/login/practice/global/filter/ExceptionFilter.java
@@ -0,0 +1,24 @@
+package com.login.practice.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
