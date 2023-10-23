a84580f8df94b7ef0653ed8b79a1aae655235c1a
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-24 00:32:22 +0900

    Feat: Security Filter Chain에 필터 추가 및 403, 401에러 대응하도록 업데이트

--- single/src/main/java/com/login/practice/global/config/SecurityConfig.java
+++ single/src/main/java/com/login/practice/global/config/SecurityConfig.java
@@ -1,19 +1,60 @@
 package com.login.practice.global.config;
 
+import com.login.practice.global.filter.ExceptionFilter;
+import com.login.practice.global.jwt.JwtAccessDeniedHandler;
+import com.login.practice.global.jwt.JwtAuthenticationEntryPoint;
+import com.login.practice.global.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtFilter jwtFilter;
+    private final ExceptionFilter exceptionFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(corsConfigurationSource());
+        http.cors().configurationSource(corsConfigurationSource()); // CORS 관련 설정
+
+        http.httpBasic().disable()
+                .csrf().disable() // REST API이므로 CSRF 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable(); // 스프링 기본 로그인 폼 미사용하므로 비활성화
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/api/").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint);
+
+        /*
+        JWT 필터링을 위한 JwtFilter를 추가
+        JwtFilter에서 발생하는 Exception은 servlet보다 앞인 filter에서 발생하므로 DispatchServlet 내의 Exception을 처리하는 ExceptionHandler로는 처리 불가능
+        JwtFilter에서 발생하는 Exception을 처리하기 위한 ExceptionFilter 추가
+         */
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionFilter, JwtFilter.class);
+
 
         return http.build();
     }
@@ -21,6 +62,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
