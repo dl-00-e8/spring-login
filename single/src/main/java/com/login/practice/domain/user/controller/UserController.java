0b8c74d0998c6d00989b21e3e552423fc20589f9
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-23 13:57:50 +0900

    Style: API endpoint 수정

--- single/src/main/java/com/login/practice/domain/user/controller/UserController.java
+++ single/src/main/java/com/login/practice/domain/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class UserController {
 
     private final UserService userService;
