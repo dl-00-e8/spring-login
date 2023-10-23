0dd782d8b37610490dcc8fc888bdb10353b501df
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-24 01:20:53 +0900

    Feat: User의 이메일과 역할을 기반으로 User 정보가 존재하는지 확인하는 메소드 개발

--- single/src/main/java/com/login/practice/domain/user/repository/UserRepository.java
+++ single/src/main/java/com/login/practice/domain/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.login.practice.domain.user.repository;
 
 import com.login.practice.domain.user.entity.User;
+import com.login.practice.domain.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailAndRole(String email, UserRole role);
 }
